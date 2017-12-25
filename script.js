f0da15f772005a07a076399240fdf0a7f74437b7
xza <584788096@qq.com>
2017-12-25 20:55:54 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
-var allButtons = $('#buttons > span');
-var n = 0;
-for(let i=0;i<allButtons.length;i++){
-  allButtons.eq(i).on('click',function(x){
-    var num = $(x.currentTarget).index();
-    n = num;
-    var p = (num * -300) + 'px';
-    $('#images').css({
-      transform:'translateX(' + p + ') translateZ(0)'
+(function(){
+  var allButtons = $('#buttons > span');
+  var n = 0;
+  var size = allButtons.length;
+  for(let i=0;i<size;i++){
+    allButtons.eq(i).on('click',function(x){
+      var num = $(x.currentTarget).index();
+      n = num;
+      var p = (num * -300) + 'px';
+      $('#images').css({
+        transform:'translateX(' + p + ') translateZ(0)'
+      });
+      $(x.currentTarget).addClass('red').siblings('.red').removeClass('red')
     });
-    $(x.currentTarget).addClass('red').siblings('.red').removeClass('red')
+  }
+  
+  function setTime() {
+    return setInterval(()=>{
+    n++;
+    allButtons.eq(n % size).trigger('click');
+    },2000);
+  }
+  
+  var stopTime = setTime();
+  
+  $('.window').on('mouseenter',function(){
+    window.clearInterval(stopTime);
   });
-}
-
-function setTime() {
-  return setInterval(()=>{
-  n++;
-  allButtons.eq(n%4).trigger('click');
-  },1000);
-}
-
-var stopTime = setTime();
-
-$('.window').on('mouseenter',function(){
-  window.clearInterval(stopTime);
-});
-$('.window').on('mouseleave',function(){
-  stopTime=setTime();
-});
+  $('.window').on('mouseleave',function(){
+    stopTime = setTime();
+  });
+})()
